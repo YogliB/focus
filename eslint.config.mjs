c13be10e954ceac92cbd3cc14e3341faea8880a8
YogliB <yogev.boaronben-har@cyberark.com>
2025-10-14 17:35:52 +0300

    chore: update eslint and add eslint-config-expo to package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,12 @@
 import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import expoConfig from 'eslint-config-expo/flat';
 
 export default defineConfig(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
+	expoConfig,
+	eslintPluginPrettierRecommended,
 );
