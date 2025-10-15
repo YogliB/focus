92fb8e83294190cddfcbe8e5d3948873d827d78b
YogliB <yogev.boaronben-har@cyberark.com>
2025-10-15 22:53:58 +0300

    chore(eslint): add Storybook plugin configuration to ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
 // @ts-check
 
-import eslint from '@eslint/js';
+import storybook from 'eslint-plugin-storybook';
+import { configs as jsConfigs } from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import { configs as tsConfigs } from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import expoConfig from 'eslint-config-expo/flat.js';
 
 export default defineConfig(
-	eslint.configs.recommended,
+	jsConfigs.recommended,
 	tsConfigs.recommended,
 	expoConfig,
+	storybook.configs['flat/recommended'],
 	eslintPluginPrettierRecommended,
 );
