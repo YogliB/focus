e8c6f8c8b83b1e698d79b72f182655acab5af76b
YogliB <yogev.boaronben-har@cyberark.com>
2025-10-15 23:40:07 +0300

    feat: Add Header and Page components with Storybook stories
    - Created Header component with user login/logout functionality.
    - Implemented Page component to demonstrate Header usage.
    - Added Storybook stories for both Header and Page components.
    - Included various assets such as images and SVGs for documentation and examples.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
 // @ts-check
 
 import storybook from 'eslint-plugin-storybook';
-import { configs as jsConfigs } from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import { configs as tsConfigs } from 'typescript-eslint';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import expoConfig from 'eslint-config-expo/flat.js';
 
+const { configs: eslintConfigs } = eslint;
+
 export default defineConfig(
-	jsConfigs.recommended,
+	globalIgnores([
+		'!.storybook/**',
+		'!.rnstorybook/**',
+		'.rnstorybook/storybook.requires.ts',
+	]),
+	eslintConfigs.recommended,
 	tsConfigs.recommended,
 	expoConfig,
 	storybook.configs['flat/recommended'],
 	eslintPluginPrettierRecommended,
+	{ rules: { 'prettier/prettier': ['error'] } },
 );
